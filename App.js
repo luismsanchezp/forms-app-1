78b3f3b074b44bdd28c0fc56bca133c9f150bed9
Luis Miguel Sanchez Pinilla <luism.sanchezp@UAM.LOCAL>
2023-03-07 19:27:51 -0500

    Add base files

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import {
+    Pressable,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    Modal
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [modalVisible, setModalVisible] = useState(false);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Registrate en la {""}
+                <Text style={styles.titleBold}>UAM</Text>
+            </Text>
+            <Pressable 
+                style={styles.btnNewUser}
+                onPress={() => setModalVisible(true)}
+            >
+                <Text style={styles.titleButton}>Press Me</Text>
+            </Pressable>
+
+            <Modal animationType="slide" visible={modalVisible}>
+                <Text>Modal</Text>
+            </Modal>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#0069a3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 22,
+        color: '#fff',
+    },
+    titleBold: {
+        fontWeight: '900',
+        color: "#f4d73b",
+    },
+    btnNewUser: {
+        backgroundColor: '#f4d73b',
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 30,
+        marginHorizontal: 20,
+    },
+    titleButton: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: '#000000',
+    },
 });
